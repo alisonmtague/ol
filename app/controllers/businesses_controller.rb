9fd1cd19c52b03695a51d9db3fd8dce868c62b8f
Alison Tague <alisonmtague@gmail.com>
2016-05-24 19:16:27 -0400

    Refactor to load db once and display status codes

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -2,26 +2,43 @@ class BusinessesController < ApplicationController
   require 'csv'
   require 'byebug'
 
-  before_action :parse_csv, only: :create
+  before_action :check_for_database, only: [:index, :show]
 
   def index
-    @businesses = Business.order('id ASC').paginate(:page => params[:page], per_page: 50)
-    render :json => @businesses
+    business_data = Business.order('id ASC').paginate(:page => params[:page], per_page: 50)
+    render json: { status: 200, business: business_data }
   end
 
-  def create
-    Business.create(business_params)
+  def create_database
+    parse_csv
+
+    business_data = Business.new#(business_params)
+    if business_data.save
+      redirect_to action: "index"
+    else
+      render json: {
+        status: 500,
+        message: "Something went wrong."
+      }.to_json
+    end
   end
 
   def show
-    @business = Business.find(params[:id])
-    render :json => @business
+    business_data = Business.find(params[:id])
+    if business_data
+      render json: { status: 200, business: business_data}
+    else
+      render json: { status: 500, message: "Something went wrong." }
+    end
+  end
+
+  def check_for_database
+    create_database if !Business.any?
   end
 
 private
 
 def parse_csv
-  Business.destroy_all
   csv_file = File.read('./businesses.csv')
   csv = CSV.parse(csv_file, :headers => true)
   csv.each do |row|
@@ -29,8 +46,8 @@ def parse_csv
   end
 end
 
-def business_params
-  params.require(:business).permit(:uuid, :name, :address, :address2, :city, :zip, :state, :country, :phone, :website, :created_at)
-end
+# def business_params
+#   params.require(:business)#.permit(:uuid, :name, :address, :address2, :city, :zip, :state, :country, :phone, :website, :created_at)
+# end
 
 end
